e5d20eacadc880861f8c9a08501f5e9ea8cd287f
Itamar Levin <itamar@oribi.io>
2017-07-21 01:12:14 +0300

    Implemented TimeFormatExtractionFn

--- src/main/java/com/github/levin81/daelic/druid/dimension/extractionfn/TimeFormatExtractionFn.java
+++ src/main/java/com/github/levin81/daelic/druid/dimension/extractionfn/TimeFormatExtractionFn.java
@@ -1,12 +1,106 @@
 package com.github.levin81.daelic.druid.dimension.extractionfn;
 
-// TODO: implement this
+import com.github.levin81.daelic.druid.granularity.Granularity;
+
+import java.time.ZoneId;
+import java.util.Locale;
+
 public class TimeFormatExtractionFn implements ExtractionFn {
 
     private final String type = "timeFormat";
 
+    private String format;
+    private ZoneId timeZone;
+    private Locale locale;
+    private Granularity granularity;
+    private boolean asMillis;
+
+    TimeFormatExtractionFn(String format, ZoneId timeZone, Locale locale, Granularity granularity, boolean asMillis) {
+        this.format = format;
+        this.timeZone = timeZone;
+        this.locale = locale;
+        this.granularity = granularity;
+        this.asMillis = asMillis;
+    }
+
     @Override
     public String getType() {
         return type;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public ZoneId getTimeZone() {
+        return timeZone;
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public Granularity getGranularity() {
+        return granularity;
+    }
+
+    public boolean isAsMillis() {
+        return asMillis;
+    }
+
+    public static TimeFormatExtractionFnBuilder builder() {
+        return new TimeFormatExtractionFnBuilder();
+    }
+
+    public static class TimeFormatExtractionFnBuilder {
+
+        private String format;
+        private ZoneId timeZone;
+        private Locale locale;
+        private Granularity granularity;
+        private boolean asMillis;
+
+        TimeFormatExtractionFnBuilder() {
+
+        }
+
+        public TimeFormatExtractionFnBuilder withFormat(String format) {
+            this.format = format;
+            return this;
+        }
+
+        public TimeFormatExtractionFnBuilder withTimeZone(String timeZone) {
+            this.timeZone = ZoneId.of(timeZone);
+            return this;
+        }
+
+        public TimeFormatExtractionFnBuilder withTimeZone(ZoneId timeZone) {
+            this.timeZone = timeZone;
+            return this;
+        }
+
+        public TimeFormatExtractionFnBuilder withLocale(String locale) {
+            this.locale = Locale.forLanguageTag(locale);
+            return this;
+        }
+
+        public TimeFormatExtractionFnBuilder withLocale(Locale locale) {
+            this.locale = locale;
+            return this;
+        }
+
+        public TimeFormatExtractionFnBuilder withGranularity(Granularity granularity) {
+            this.granularity = granularity;
+            return this;
+        }
+
+        public TimeFormatExtractionFnBuilder withAsMillis(boolean asMillis) {
+            this.asMillis = asMillis;
+            return this;
+        }
+
+        public TimeFormatExtractionFn build() {
+            return new TimeFormatExtractionFn(format, timeZone, locale, granularity, asMillis);
+        }
     }
 }
